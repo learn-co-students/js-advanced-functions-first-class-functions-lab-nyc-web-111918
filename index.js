8f05653ef355ba4515ea621b9a54f25602daeea7
Tonyjlum <tonyjlum@gmail.com>
2019-01-07 14:53:23 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(drivers.length-2)
+}
+
+const selectingDrivers = [
+  returnFirstTwoDrivers,
+  returnLastTwoDrivers
+]
+
+function createFareMultiplier (fare){
+  return function(multi){
+    return fare * multi
+  }
+}
+
+const fareDoubler = function(fare){
+  return fare * 2
+}
+
+const fareTripler = function(fare){
+  return fare * 3
+}
+
+function fetchSpecifiedDrivers(drivers, obj){ 
+  return obj(drivers)
+}
